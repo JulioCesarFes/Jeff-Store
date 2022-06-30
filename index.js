805ac74b145bd241b979193f759482110262cba3
Julio Ferreira <julio@compliasset.com>
2022-06-30 17:55:41 -0300

    auth frist commit

--- index.js
+++ index.js
@@ -1,26 +1,35 @@
 const express = require('express');
 const mysql = require('mysql');
 const mysql_config = require('./mysql_config');
-
-console.log(mysql_config)
-
 const con = mysql.createConnection(mysql_config);
 
+const {cryptPassword, comparePassword} = require('./password_encryption');
+
 const app = express();
+app.use(express.json());
 app.listen(3000, () => console.log('Example app is listening on port 3000.'));
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
 
-app.get('/', (req, res) => {
+app.post('/register', function (req, res) {
+  console.log(req.body)
+  res.send('hi')
+})
 
-  con.query("SELECT * FROM jeffs", function (err, result, fields) {
-    if (err) throw err;
-    console.log(result);
-  });
 
-  res.send('Successful response.');
-});
 
+cryptPassword('123456', (err, hash) => {
+  console.log(err, hash)
+
+  comparePassword('123456', hash, (err, a2) => {
+    console.log('123456', err, a2)
+  })
+
+  comparePassword('111111', hash, (err, a2) => {
+    console.log('111111', err, a2)
+  })
+
+  comparePassword('', hash, (err, a2) => {
+    console.log('', err, a2)
+  })
+
+})
