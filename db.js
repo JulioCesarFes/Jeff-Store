fd3e0bba2e913b1c9477c270a4352d9833259d5a
Julio Ferreira <julio@compliasset.com>
2022-07-01 17:42:06 -0300

    db js

---
+++ db.js
@@ -0,0 +1,30 @@
+const mysql = require('mysql2')
+const mysql_config = require('./mysql_config')
+const con = mysql.createConnection(mysql_config)
+
+con.query(`DROP DATABASE ${mysql_config.database};`, (err, result, fields) => {
+  if (err) throw err
+  console.log('dropped database')
+})
+
+con.query(`CREATE DATABASE ${mysql_config.database};`, (err, result, fields) => {
+  if (err) throw err;
+  console.log('created database')
+})
+
+con.query(`USE ${mysql_config.database};`, (err, result, fields) => {
+  if (err) throw err;
+  console.log('using database')
+})
+
+con.query(`
+  CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL, 
+    username VARCHAR(255) NOT NULL, 
+    password VARCHAR(255) NOT NULL, 
+    PRIMARY KEY (id)
+  );
+`, (err, result, fields) => {
+  if (err) throw err
+  console.log('created table users')
+})
