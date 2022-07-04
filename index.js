e18283482706f6e0b0ef677c21341acd56156300
Julio Ferreira <julio@compliasset.com>
2022-07-04 17:58:36 -0300

    organizing routes

--- index.js
+++ index.js
@@ -13,22 +13,24 @@ app.listen(3000, () => console.log('Example app is listening on port 3000.'))
 app.post('/register', function (req, res) {
   let {username, password} = req.body
   
-  cryptPassword(password, (err, hash) => {
+  cryptPassword(password, (err, passhash) => {
     if (err) {
-      res.status(500).send('num deu')
-    } else {
-
-      password = hash
-
-      let args = [username, password]
-      con.execute('INSERT INTO `users`(`username`, `password`) VALUES (?, ?)', args, (err, result, fields) => {
-        if (err) {
-          res.status(500).send('se vira')
-        } else {
-          res.status(201).send(`deu c: ... O usuário ${username} foi criado com sucesso!`)
-        }
-      })
+      res.status(500).send('Server Internal Error')
+      console.log(err)
+      return
     }
+
+    let args = [username, passhash]
+
+    con.execute('INSERT INTO `users`(`username`, `password`) VALUES (?, ?)', args, (err, result, fields) => {
+      if (err) {
+        res.status(500).send('Server Internal Error')
+        console.log(err)
+        return
+      }
+      
+      res.status(201).send(`deu c: ... O usuário ${username} foi criado com sucesso!`)
+    })
   })
 })
 
@@ -39,25 +41,32 @@ app.post('/login', function (req, res) {
   
   con.execute('SELECT * FROM users WHERE username = ? LIMIT 1', args, (err, result, fields) => {
     if (err) {
-      res.status(500).send('num deu')
-    } else {
-      console.log({err, result})
-
-      let user = result[0]
-      let stored_password = user.password
-
-      comparePassword(password, stored_password, (err, correct_password) => {
-        if (err) {
-          res.status(500).send('se vira')
-        } else {
-
-          if (correct_password) {
-            res.status(200).send(`Achei o cê e seu número é ${user.id}`)
-          } else {
-            res.status(404).send('User not found')
-          }
-        }
-      })
+      res.status(500).send('Server Internal Error')
+      console.log(err)
+      return
     }
+
+    if (!result.length) {
+      res.status(404).send('User not found')
+      return
+    }
+
+    let user = result[0]
+    let stored_password = user.password
+
+    comparePassword(password, stored_password, (err, correct_password) => {
+      if (err) {
+        res.status(500).send('Server Internal Error')
+        console.log(err)
+        return
+      }
+
+      if (!correct_password) {
+        res.status(404).send('User not found')
+        return
+      }
+
+      res.status(200).send(`Achei o cê e seu número é ${user.id}`)
+    })
   })
 })
